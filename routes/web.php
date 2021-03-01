9553208fd0d857a0491bc1d91b0caa88bb8eda3b
Taqin86 <63039420+Taqin86@users.noreply.github.com>
2021-03-01 19:34:21 +0700

    memperbaiki route dari home

--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,8 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', 'HomeController@index')->name('home');
 
-Route::get('/debug-sentry', function () {
-    throw new Exception('Fake Error!');
-});
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+
