c12098b73d09b4704961f35e42ee5a402603e54d
Taqin86 <63039420+Taqin86@users.noreply.github.com>
2021-03-01 19:35:57 +0700

    memisahkan satu persatu bagian

---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,13 @@
+<!-- START: Bagian Footer -->
+    <footer>
+      <div class="container">
+        <div class="row">
+          <div class="col-12 text-center">
+            <p class="pt-4 pb-2">
+              2021 Copyright Toko Online. All Rights Reserved by Moh Rizqi Muttaqin.
+            </p>
+          </div>
+        </div>
+      </div>
+    </footer>
+    <!-- END: Bagian Footer -->
