77165abdf4422d53e2b17d35776331e61b320d05
Taqin86 <63039420+Taqin86@users.noreply.github.com>
2021-03-04 02:50:37 +0700

    update $customer, $revenue, $transaction

--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -22,7 +22,7 @@
             <div class="card mb-2">
                 <div class="card-body">
                 <div class="dashboard-card-title">Customer</div>
-                <div class="dashboard-card-subtitle">15,209</div>
+                <div class="dashboard-card-subtitle">{{ $customer }}</div>
                 </div>
             </div>
             </div>
@@ -30,7 +30,7 @@
             <div class="card mb-2">
                 <div class="card-body">
                 <div class="dashboard-card-title">Revenue</div>
-                <div class="dashboard-card-subtitle">$931,290</div>
+                <div class="dashboard-card-subtitle">${{ $revenue }}</div>
                 </div>
             </div>
             </div>
@@ -38,81 +38,11 @@
             <div class="card mb-2">
                 <div class="card-body">
                 <div class="dashboard-card-title">Transaction</div>
-                <div class="dashboard-card-subtitle">22,409,399</div>
+                <div class="dashboard-card-subtitle">{{ $transaction }}</div>
                 </div>
             </div>
             </div>
             <!-- END: Card -->
-        </div>
-        <div class="row mt-3">
-            <div class="col-12 mt-2">
-            <!-- START: Recent Transaction -->
-            <h5 class="mb-3">Recent Transaction</h5>
-            <a href="/dashboard-transaction-details.html" class="card card-list d-block">
-            <div class="card-body">
-                <div class="row">
-                <div class="col-md-1">
-                    <img src="/images/dashboard-icon-product-1.png" alt="icon product 1">
-                </div>
-                <div class="col-md-4">
-                    Shirup Marzaan
-                </div>
-                <div class="col-md-3">
-                    Angga Risky
-                </div>
-                <div class="col-md-3">
-                    12 Januari, 2020
-                </div>
-                <div class="col-md-1 d-none d-md-block">
-                    <img src="/images/dashboard-arrow-right.svg" alt="arrow">
-                </div>
-                </div>
-            </div>
-            </a>
-            <a href="/dashboard-transaction-details.html" class="card card-list d-block">
-                <div class="card-body">
-                <div class="row">
-                    <div class="col-md-1">
-                    <img src="/images/dashboard-icon-product-2.png" alt="icon product 2">
-                    </div>
-                    <div class="col-md-4">
-                    LeBrone X
-                    </div>
-                    <div class="col-md-3">
-                    Masayoshi
-                    </div>
-                    <div class="col-md-3">
-                    11 January, 2020
-                    </div>
-                    <div class="col-md-1 d-none d-md-block">
-                    <img src="/images/dashboard-arrow-right.svg" alt="arrow">
-                    </div>
-                </div>
-                </div>
-            </a>
-            <a href="/dashboard-transaction-details.html" class="card card-list d-block">
-                <div class="card-body">
-                <div class="row">
-                    <div class="col-md-1">
-                    <img src="/images/dashboard-icon-product-3.png" alt="icon product 3">
-                    </div>
-                    <div class="col-md-4">
-                    Soffa Lembutte
-                    </div>
-                    <div class="col-md-3">
-                    Shayna
-                    </div>
-                    <div class="col-md-3">
-                    11 January, 2020
-                    </div>
-                    <div class="col-md-1 d-none d-md-block">
-                    <img src="/images/dashboard-arrow-right.svg" alt="arrow">
-                    </div>
-                </div>
-                </div>
-            </a>
-            <!-- END: Recent Transaction -->
-            </div>
         </div>
         </div>
     </div>
