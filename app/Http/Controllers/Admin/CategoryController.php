acaaef81ac7094d6d6a614ae04f3c4476eff67ee
Taqin86 <63039420+Taqin86@users.noreply.github.com>
2021-03-05 02:19:03 +0700

    typ pada update

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -107,7 +107,7 @@ class CategoryController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function update(CaregoryRequest $request, $id)
+    public function update(CategoryRequest $request, $id)
     {
         //
     }
