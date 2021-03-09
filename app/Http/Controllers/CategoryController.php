cccbbd17fe04a3c6ebf54bb337f1bfea1b6e6ee3
unknown <rizqi.muttaqi@gmail.com>
2021-03-09 03:38:32 -0500

    logika untuk category

--- app/Http/Controllers/CategoryController.php
+++ app/Http/Controllers/CategoryController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Category;
+use App\Product;
 use Illuminate\Http\Request;
 
 class CategoryController extends Controller
@@ -13,6 +15,29 @@ class CategoryController extends Controller
      */
     public function index()
     {
-        return view('pages.category');
+        $categories = Category::all();
+        $products = Product::with(['galleries'])->pagination(32);
+
+        return view('pages.home',[
+            'categories' => $categories,
+            'products' => $products
+        ]);
+    }
+
+    /**
+     * Show the application dashboard.
+     *
+     * @return \Illuminate\Contracts\Support\Renderable
+     */
+    public function detail(Request $request, $slug)
+    {
+        $categories = Category::all();
+        $category = Category::where('slug', $slug)->firstOrFail();
+        $products = Product::with(['galleries'])->where('categories_id', $category->id)->pagination(32);
+
+        return view('pages.home',[
+            'categories' => $categories,
+            'products' => $products
+        ]);
     }
 }
