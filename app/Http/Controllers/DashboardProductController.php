9c84045c66f3d816d5217e04b4d056b72cb9a124
Taqin86 <63039420+Taqin86@users.noreply.github.com>
2021-03-10 05:05:39 +0700

    logic store

--- app/Http/Controllers/DashboardProductController.php
+++ app/Http/Controllers/DashboardProductController.php
@@ -2,9 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\User;
 use App\Product;
+use App\Category;
+use Illuminate\Support\Str;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Http\Requests\Admin\ProductRequest;
+use App\ProductGallery;
 
 class DashboardProductController extends Controller
 {
@@ -24,6 +29,25 @@ class DashboardProductController extends Controller
 
     public function create()
     {
-        return view('pages.dashboard-products-create');
+        $categories = Category::all();
+        return view('pages.dashboard-products-create',[
+            'categories' => $categories
+        ]);
+    }
+
+    public function store(ProductRequest $request)
+    {
+        $data = $request->all();
+        
+        $data['slug'] = Str::slug($request->name);
+        $product = Product::create($data);
+
+        $gallery = [
+            'products_id' => $product->id,
+            'photos' => $request->file('photo')->store('assets/product', 'public')
+        ];
+        ProductGallery::create($gallery);
+
+        return redirect()->route('dashboard-product');
     }
 }
