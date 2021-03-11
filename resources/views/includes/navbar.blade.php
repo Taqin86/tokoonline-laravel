66f94ddd0ccca88ae05c01c5f7640859ae90bcba
Taqin86 <63039420+Taqin86@users.noreply.github.com>
2021-03-12 05:01:59 +0700

    typo

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -64,10 +64,13 @@
                   >Settings</a
                 >
                 <div class="dropdown-divider"></div>
-                <a href="{{ route('logout') }}" onclick="event.preventDefault(); document.getElementById('logout-form').submit();" 
-                class="dropdown-item">
-                Logout
-              </a>
+                <a class="dropdown-item" href="{{ route('logout') }}"
+                  onclick="event.preventDefault(); document.getElementById('logout-form').submit();">
+                  Logout
+                </a>
+                <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
+                    @csrf
+                </form>
               </div>
             </li>
             <li class="nav-item">
