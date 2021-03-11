082641a85e568a5f2ddca8354033fc8c081b9042
Taqin86 <63039420+Taqin86@users.noreply.github.com>
2021-03-12 05:01:45 +0700

    daftar user

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -174,45 +174,6 @@
         el: '#register',
         mounted() {
           AOS.init();
-        
-<<<<<<< HEAD
-        },
-        methods: {
-            checkForEmailAvailability: function() {
-                var self = this;
-                axios.get('{{ route('api-register-check') }}', {
-                    params: {
-                        email: this.email
-                    }
-                })
-                    .then(function (response) {
-                        if(response.data == 'Available') {
-                            self.$toasted.show(
-                                "Email anda tersedia! Silahkan lanjut langkah selanjutnya!",
-                                {
-                                position: "top-center",
-                                className: "rounded",
-                                duration: 1000
-                                }
-                            );
-                            self.email_unavailable = false;
-                        } else {
-                            self.$toasted.error(
-                                "Maaf, tampaknya email sudah terdaftar pada sistem kami.",
-                                {
-                                position: "top-center",
-                                className: "rounded",
-                                duration: 1000
-                                }
-                            );
-                            self.email_unavailable = true;
-                        }
-                        // handle success
-                        console.log(response);
-                    });
-            }
-        },
-=======
         },
         methods: {
             checkForEmailAvailability: function() {
@@ -250,7 +211,6 @@
                     });
             }
         },
->>>>>>> 706837785c1905271e09d980b6a8aa0b8f36161d
         data() {
             return {
                 name: "Angga Rizqi Sett",
