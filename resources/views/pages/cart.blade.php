9c2915d35344cf62c6f3e478500a361cd46abc23
Taqin86 <63039420+Taqin86@users.noreply.github.com>
2021-03-17 15:51:53 +0700

    menambhkn link checkout dan total price

--- resources/views/pages/cart.blade.php
+++ resources/views/pages/cart.blade.php
@@ -94,7 +94,9 @@
             </div>
             <!-- END: Shipping Details -->
           </div>
-          <form action="" id="locations">
+          <form action="{{ route('checkout') }}" id="locations" enctype="multipart/form-data" method="POST">
+            @csrf
+            <input type="hidden" name="total_price" value="{{ $totalPrice }}">
             <div class="row mb-2" data-aos="fade-up" data-aos-delay="200">
               <!-- SRART: Form Table Shipping Details -->
               <div class="col-md-6">
@@ -177,7 +179,10 @@
                 <div class="product-subtitle">Total</div>
               </div>
               <div class="col-8 col-md-3">
-                <a href="/success.html" class="btn btn-success mt-4 px-4 btn-block">Checkout Now</a>
+                <button 
+                type="submit" class="btn btn-success mt-4 px-4 btn-block">
+                  Checkout Now
+                </button>
               </div>
             </div>
           </form>
