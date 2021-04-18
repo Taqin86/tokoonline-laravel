e4281165a5baeb767da003c0e884f5b0cce17c98
Taqin86 <63039420+Taqin86@users.noreply.github.com>
2021-04-18 09:21:49 +0700

    menambhakn typo dan function details

--- app/Http/Controllers/DashboardTransactionController.php
+++ app/Http/Controllers/DashboardTransactionController.php
@@ -18,12 +18,12 @@ class DashboardTransactionController extends Controller
         $sellTransactions = TransactionDetail::with(['transaction.user','product.galleries'])
                             ->whereHas('product', function($product){
                                 $product->where('users_id', Auth::user()->id);
-                            });
+                            })->get();
 
         $buyTransactions = TransactionDetail::with(['transaction.user','product.galleries'])
                             ->whereHas('transaction', function($transaction){
                                 $transaction->where('users_id', Auth::user()->id);
-                            });
+                            })->get();
 
         return view('pages.dashboard-transactions',[
             'sellTransactions' => $sellTransactions,
@@ -31,8 +31,24 @@ class DashboardTransactionController extends Controller
         ]);
     }
 
-    public function details()
+    public function details(Request $request, $id)
     {
-        return view('pages.dashboard-transactions-details');
+        $transaction = TransactionDetail::with(['transaction.user','product.galleries'])
+                            ->findOrFail($id);
+
+        return view('pages.dashboard-transactions-details',[
+            'transaction' => $transaction
+        ]);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $data = $request->all();
+
+        $item = TransactionDetail::findOrFail($id);
+
+        $item->update($data);
+
+        return redirect()->route('dashboard-transaction-details', $id);
     }
 }
